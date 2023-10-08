ac9327b4091bfdfff23c4b3281f0a9950408cf2e
idlenonsense <rusbelousov131@gmail.com>
2023-10-08 14:37:44 +0300

    fixed the stuff

--- src/main/java/net/lidlez/theseaofsilence/block/AncientPortalBlock.java
+++ src/main/java/net/lidlez/theseaofsilence/block/AncientPortalBlock.java
@@ -1,22 +1,24 @@
 package net.lidlez.theseaofsilence.block;
 
-import jdk.jfr.Enabled;
-import net.minecraft.core.Direction;
+import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.StateDefinition;
-import net.minecraft.world.level.block.state.properties.BlockStateProperties;
 import net.minecraft.world.level.block.state.properties.BooleanProperty;
 
 public class AncientPortalBlock extends Block {
-
-    public static final BooleanProperty IS_ACTIVE = BlockStateProperties.ENABLED;
+    public static final BooleanProperty IS_ACTIVE = BooleanProperty.create("is_active");
+    public static final BooleanProperty ACTIVE = IS_ACTIVE;
 
     public AncientPortalBlock(Properties pProperties) {
         super(pProperties);
     }
+
     protected void createBlockStateDefinition(StateDefinition.Builder<Block, BlockState> pBuilder) {
-        pBuilder.add(IS_ACTIVE);
+        pBuilder.add(ACTIVE);
+    }
+
+    public BlockState getStateForPlacement(BlockPlaceContext pContext) {
+        return this.defaultBlockState().setValue(ACTIVE, Boolean.valueOf(false));
     }
 }
--- src/main/java/net/lidlez/theseaofsilence/item/StaffOfSilence.java
+++ src/main/java/net/lidlez/theseaofsilence/item/StaffOfSilence.java
@@ -2,17 +2,13 @@ package net.lidlez.theseaofsilence.item;
 
 import net.lidlez.theseaofsilence.block.AncientPortalBlock;
 import net.lidlez.theseaofsilence.block.ModBlocks;
-import net.lidlez.theseaofsilence.datagen.ModBlockStateProvider;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.InteractionResult;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.item.context.UseOnContext;
 import net.minecraft.world.level.Level;
-import net.minecraft.world.level.block.Block;
-import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockState;
-
 
 
 public class StaffOfSilence extends Item {
@@ -22,25 +18,24 @@ public class StaffOfSilence extends Item {
     }
 
     @Override
-
     public InteractionResult useOn(UseOnContext pContext) {
         Level level = pContext.getLevel();
         BlockPos blockpos = pContext.getClickedPos();
         BlockState blockstate = level.getBlockState(blockpos);
-        Player player  = pContext.getPlayer();
-
-
-        if(blockstate.is(Blocks.BEDROCK) && !blockstate.getValue(AncientPortalBlock.IS_ACTIVE)){
-
-            return InteractionResult.SUCCESS;
-
-        }
-        else{
+        Player player = pContext.getPlayer();
+        if (blockstate.is(ModBlocks.ANCIENT_PORTAL.get()) && !blockstate.getValue(AncientPortalBlock.ACTIVE)) {
+            if (level.isClientSide) {
+                return InteractionResult.SUCCESS;
+            } else {
+                player.setHealth(10f);
+                BlockState blockstate1 = blockstate.setValue(AncientPortalBlock.ACTIVE, Boolean.valueOf(true));
+                level.setBlock(blockpos, blockstate1, 2);
+                pContext.getItemInHand().shrink(1);
+                }
+                return InteractionResult.CONSUME;
+        } else {
             return InteractionResult.PASS;
         }
-
-
-
     }
 
 
